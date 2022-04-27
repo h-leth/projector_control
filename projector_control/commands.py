df83767d90825534261f3c78c14a82c6f03d68ed
Henning Leth <leth.henning@gmail.com>
2022-04-27 21:04:23 +0200

    Remove debug print statements and fix typo

--- projector_control/commands.py
+++ projector_control/commands.py
@@ -7,66 +7,56 @@ from vars import auth, ip_addr
 
 
 def power_on():
-    url = f'{ip_addr}/cgi-bin/power_ctl.cgi?key=pow_on&lang=e'
-#    requests.get(url, auth=auth)
-    print('power on')
+    url = f'{ip_addr}/cgi-bin/power_on.cgi'
+    requests.post(url, auth=auth)
 
 def power_off():
-    url = f'{ip_addr}/cgi-bin/power_ctl.cgi?key=pow_off&lang=e'
-#    requests.get(url, auth=auth)
-    print('power off')
+    url = f'{ip_addr}/cgi-bin/power_off.cgi'
+    requests.post(url, auth=auth)
 
 
 def shutter_open():
-    url = f'{ip_addr}/cgi-bin/power_ctl.cgi?key=shutter_off&lang=e'
-#    requests.get(url, auth=auth)
-    print('shutter open')
+    url = f'{ip_addr}/cgi-bin/proj_ctl.cgi?key=shutter_off&lang=e&osd=on'
+    requests.get(url, auth=auth)
 
 
 def shutter_close():
-    url = f'{ip_addr}/cgi-bin/power_ctl.cgi?key=shutter_on&lang=e'
-#    requests.get(url, auth=auth)
-    print('shutter close')
+    url = f'{ip_addr}/cgi-bin/proj_ctl.cgi?key=shutter_on&lang=e&osd=one'
+    requests.get(url, auth=auth)
 
 
 def vshift_inc(x):
     url = f'{ip_addr}/cgi-bin/proj_ctl.cgi?key=lens_vshift_inc1&lang=e'
-#    for i in range(x):
-#       requests.get(url, auth=auth)
-#       sleep(.05)
-    print('up')
+    for i in range(x):
+       requests.get(url, auth=auth)
+       sleep(.05)
 
 
 def vshift_dec(x):
     url = f'{ip_addr}/cgi-bin/proj_ctl.cgi?key=lens_vshift_dec1&lang=e'
-#    for i in range(x):
-#       requests.get(url, auth=auth)
-#        sleep(.05)
-    print('down')
+    for i in range(x):
+       requests.get(url, auth=auth)
+       sleep(.05)
 
 def zoom_inc(x):
     url = f'{ip_addr}/cgi-bin/proj_ctl.cgi?key=lens_zoom_inc1&lang=e'
-#    for i in range(x):
-#        requests.get(url, auth=auth)
-#        sleep(.05)
-    print('left')
+    for i in range(x):
+        requests.get(url, auth=auth)
+        sleep(.05)
 
 def zoom_dec(x):
     url = f'{ip_addr}/cgi-bin/proj_ctl.cgi?key=lens_zoom_dec1&lang=e'
-#    for u in range(x):
-#        requests.get(url, auth=auth)
-#        sleep(.05)
-    print('right')
+    for i in range(x):
+        requests.get(url, auth=auth)
+        sleep(.05)
 
 def backwall():
-    print('backwall')
-#    zoom_dec(75)
-#    vshift_dec(100)
+    zoom_dec(75)
+    vshift_dec(100)
     write_entry(backwall.__name__)
 
 
 def screen():
-    print('screen')
-#    vshift_inc(100)
-#    zoom_inc(75)
+    vshift_inc(100)
+    zoom_inc(75)
     write_entry(screen.__name__)
